f8b87ad6f6315c4778f1defd620e6290d4a8b26e
Bloodyaugust <greysonrichey@gmail.com>
2016-04-16 14:44:24 -0600

    Adding a new Game scene, setting up a super basic main menu with control object and script.

---
+++ Assets/Scripts/MainMenuControl.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuControl : MonoBehaviour {
+
+	public void StartGame () {
+		Application.LoadLevel("Game");
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
